36831a2388594e31f05798914ad33e857768b9e5
tobiascrocus <tobiascrocus@hotmail.com>
2022-04-18 18:54:29 -0300

    Terminacion de estilos en historia y juego

---
+++ script.js
@@ -0,0 +1,11 @@
+let video = document.querySelector('video');
+let botonPlay = document.getElementById("botonPlay");
+let botonPause = document.getElementById("botonPause");
+
+botonPlay.addEventListener('click',()=>{
+    video.play();
+});
+
+botonPause.addEventListener('click',()=>{
+    video.pause();
+});
